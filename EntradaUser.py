362d232f7d3103e694f9a71e3bd38fbb36e6b142
Victor Hugo C. Gomes <vhclaudiogomes@gmail.com>
2023-07-05 22:45:01 -0300

    VH | Adicionado a entrada de usuario

--- EntradaUser.py
+++ EntradaUser.py
@@ -1,14 +1,15 @@
 from collections import defaultdict
 
 def entrada_do_usuario():
-    grafo = defaultdict(list)
+    grafo = {}
 
     arestas = int(input("Digite o número de arestas do grafo: "))
 
     for i in range(arestas):
-        aresta = input(f"Digite a aresta {i+1} no formato 'origem destino1 destino2 ... : ")
-        origem, *destino = aresta.split()
-        grafo[origem].append(destino)
+        aresta = input(f"Digite a aresta {i+1} no formato 'origem destino1 destino2 ...': ")
+        origem, *destinos = aresta.split()
+        grafo[origem] = destinos
+
 
     print(grafo)
     return grafo
